2735af8d1bdbfcd62dd93b5a9fb806e40eab0ddf
Rachel Kendall <rparsons@cardinalsolutions.com>
2016-04-05 09:21:08 -0400

    updated waypoint converter to generate output.js

---
+++ output.js
@@ -0,0 +1,24 @@
+var target = UIATarget.localTarget();
+var mainWindow = target.frontMostApp().mainWindow();
+var points = [
+{ location : { latitude : 35.5416844, longitude : -80.8550356 }, 
+ options: {speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15} }
+{ location : { latitude : 35.5429996, longitude : -80.8560598 }, 
+ options: {speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15} }
+{ location : { latitude : 35.5338927, longitude : -80.8561295 }, 
+ options: {speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15} }
+{ location : { latitude : 35.531376, longitude : -80.860818 }, 
+ options: {speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15} }
+{ location : { latitude : 35.5335522, longitude : -80.861561 }, 
+ options: {speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15} }
+{ location : { latitude : 35.5634446, longitude : -80.8583722 }, 
+ options: {speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15} }
+{ location : { latitude : 35.5825561, longitude : -80.8568251 }, 
+ options: {speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15} }
+{ location : { latitude : 35.5911214, longitude : -80.8591631 }, 
+ options: {speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15} }
+{ location : { latitude : 35.5938537, longitude : -80.8591211 }, 
+ options: {speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15} }
+{ location : { latitude : 35.593284, longitude : -80.866262 }, 
+ options: {speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15} }
+];
--- waypointconverter.py
+++ waypointconverter.py
@@ -8,5 +8,26 @@ root = tree.getroot()
 
 namespace = {"ns" : "http://www.topografix.com/GPX/1/1"}
 
-for waypoint in root.findall('ns:wpt', namespace):
-    print waypoint.attrib
+newfile = open("output.js", "w")
+newfile.write("var target = UIATarget.localTarget();\n")
+newfile.write("var mainWindow = target.frontMostApp().mainWindow();\n")
+newfile.write("var points = [\n")
+
+for waypoint in root.findall("ns:wpt", namespace):
+    latlong = waypoint.attrib
+    lat = latlong["lat"]
+    lon = latlong["lon"]
+    print(lat)
+    print(lon)
+
+    pointstring = "{{ location : {{ latitude : {0}, longitude : {1} }}, \n options: {{speed:8, altitude:200, horizontalAccuracy:10, verticalAccuracy:15}} }}"
+    formattedPoint = pointstring.format(lat, lon)
+    print formattedPoint
+
+    newfile.write(formattedPoint)
+    newfile.write("\n")
+
+newfile.write("];")
+newfile.close()
+
+print("all done!")
